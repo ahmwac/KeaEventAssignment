60874333516b136577ab80b1eadc0f9f45a7b8eb
Ali <Alighali92@hotmail.com>
2017-09-29 12:04:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const path = __dirname + '/public/';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('wwwroot'));
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello from root</h1>');
+});
+
+app.get('/testdigselv', function (req, res) {
+    res.send('Du vandt!!');
+});
+
+app.get('/login', function (req, res) {
+    res.sendFile(path + 'login.html', function (err) {
+        // handle error
+    });
+});
+
+app.post('/login', function (req, res) {
+    if (req.body.user === 'ali') {
+        res.sendFile(path + 'index.html', function (err) {
+        });
+    } else {
+        res.sendFile(path + 'login.html', function (err) {
+            // handle error
+        });
+    }
+});
+
+app.get('/index', function (req, res) {
+    res.sendFile(path + 'index.html', function (err) {
+        // handle error
+    });
+});
+
+app.listen(3000);
